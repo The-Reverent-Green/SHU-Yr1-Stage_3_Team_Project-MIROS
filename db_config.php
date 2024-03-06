04d3b146bb85cd0d5b82e190472433c392ec2fb8
Gceorge <george.colley@student.shu.ac.uk>
2024-03-06 11:01:59 +0000

    commit

--- db_config.php
+++ db_config.php
@@ -1,1 +1,17 @@
-#enter details for database connection
+<?php
+$servername = "localhost"; // or your MySQL server address
+$username = "your_username"; // your MySQL username
+$password = "your_password"; // your MySQL password
+$database = "enigma_inc"; // your MySQL database name
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
+
+//configure above with database name and password etc
