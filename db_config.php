0cf7b6599c409a65dac3d7a4524be19aaf9d1fbe
The-Reverent-Green <132163747+The-Reverent-Green@users.noreply.github.com>
2024-03-06 13:04:35 +0000

    Optimised unoptimal code.

--- db_config.php
+++ db_config.php
@@ -1,14 +1,18 @@
-<?php
+<?php  // db_config.php
+
 $servername = "localhost";
 $username = "your_username"; 
 $password = ""; 
 $database = "databasename"; 
 //configure above with database name and password etc
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to $database successfully"; 
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+try{
+    $dbConnection = new PDO("mysql:host=$servername; dbname=$database; charset=utf8", 
+    $username, 
+    $password);
+    //var_dump($dbConnection);
+    echo '$Connection Successful.<br>';
+}catch(Exception $e){
+    //echo $e->getMessage();
+    echo 'An error has occurred:';
 }
