752705f4485b0b3de9a92927f26c3f337052a765
The-Reverent-Green <132163747+The-Reverent-Green@users.noreply.github.com>
2024-03-06 12:58:06 +0000

    Removed excess

--- db_config.php
+++ db_config.php
@@ -1,14 +1,13 @@
 <?php
-$servername = "localhost"; // or your MySQL server address
-$username = "your_username"; // your MySQL username
-$password = "your_password"; // your MySQL password
-$database = "databasename"; // your MySQL database name
+$servername = "localhost";
+$username = "your_username"; 
+$password = ""; 
+$database = "databasename"; 
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    echo "Connected to $database successfully"; 
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
