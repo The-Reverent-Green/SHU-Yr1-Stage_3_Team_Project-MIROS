5f67b15d33c6f2b2ffe3629deb75a71c1d7ed2f1
The-Reverent-Green <132163747+The-Reverent-Green@users.noreply.github.com>
2024-03-13 12:09:24 +0000

    dbtest.php

--- dbTest.php
+++ dbTest.php
@@ -1,6 +1,6 @@
 <?php //dbTest.php
 include "db_config.php";
-
+echo 'Testing: $dbConnection->query("CREATE DATABASE myDB");.';
 try{
     $dbConnection->query("CREATE DATABASE myDB");
     echo "Database created successfully";
@@ -8,7 +8,7 @@ try{
 }catch(Exception $e){
     echo "PDO ".$e->getMessage();
 }
-
+echo 'Testing: $query = file_get_contents("DDL.SQL");';
 try{
     $query = file_get_contents("DDL.SQL");
     if (!$query) die("Failed to read SQL file");
