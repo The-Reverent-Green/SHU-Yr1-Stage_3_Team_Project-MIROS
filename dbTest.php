332a7cc5f2d6a82c4623030ae72aba0c09a52bfc
The-Reverent-Green <132163747+The-Reverent-Green@users.noreply.github.com>
2024-03-13 11:18:25 +0000

    edited test connections

---
+++ dbTest.php
@@ -0,0 +1,19 @@
+<?php //dbTest.php
+include "db_config.php";
+
+try{
+    $dbConnection->query("CREATE DATABASE myDB");
+    echo "Database created successfully";
+    $dbConnection->close();
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
+try{
+    $query = file_get_contents("DDL.SQL");
+    if (!$query) die("Failed to read SQL file");
+    $mysqliConnection->multi_query($query);
+    $mysqliConnection->close();
+} catch(Exception $e){
+    echo $e->getMessage();
+}
--- db_config.php
+++ db_config.php
@@ -1,17 +1,24 @@
 <?php  // db_config.php
 
 $servername = "localhost";
+$database = "databasename"; 
 $username = "your_username"; 
 $password = ""; 
-$database = "databasename"; 
 //configure above with database name and password etc
 
-try{
-    $dbConnection = new PDO("mysql:host=$servername; dbname=$database; charset=utf8", 
+try{//                              PLEASE USE $pdoConnection FOR ALL DB CONNECTIONS
+    $pdoConnection = new PDO("mysql:host=$servername; dbname=$database; charset=utf8", 
     $username, 
     $password);
     //var_dump($dbConnection);
     echo '$Connection Successful.<br>';
+}catch(Exception $e){
+    //echo $e->getMessage();
+    echo 'An error has occurred:';
+}
+
+try{//                              PLEASE DON'T USE $mysqliConnection UNLESS ABSOLUTELY NECESSARY
+    $mysqliConnection = new mysqli($servername, $username, $password, $dbname);
 }catch(Exception $e){
     //echo $e->getMessage();
     echo 'An error has occurred:';
