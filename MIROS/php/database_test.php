72542cc96b4a490e26ad24fa8d782688cde4b71e
George <97513198+Gceorge@users.noreply.github.com>
2024-03-18 14:02:21 +0000

    commit

--- MIROS/php/database_test.php
+++ MIROS/php/database_test.php
@@ -1,24 +1,23 @@
 <?php
-include '../php/db_config.php';
+include 'db_config.php';
 
 if ($mysqli->connect_errno) {
     die("Connection failed: " . $mysqli->connect_error);
 }
-echo "Connected successfully to the database.";
+echo "Connected successfully to the database.<br>";
 
 $query = "SHOW TABLES FROM $dbname";
 $result = $mysqli->query($query);
 
 if ($result) {
-    echo "\n\nList of tables in the database:\n";
+    echo "<br>List of tables in the database:<ul>";
     while ($row = $result->fetch_assoc()) {
-        echo $row["Tables_in_$dbname"] . "\n";
+        echo "<li>" . $row["Tables_in_$dbname"] . "</li>";
     }
+    echo "</ul>";
 } else {
     echo "Error listing tables: " . $mysqli->error;
 }
 
 $mysqli->close();
 ?>
-
-
