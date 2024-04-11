2dbd62468b215387cb460fe755818bab49780cab
Gceorge <george.colley@student.shu.ac.uk>
2024-04-11 22:10:30 +0100

    George

--- MIROS/database/db_config.php
+++ MIROS/database/db_config.php
@@ -10,23 +10,18 @@ $password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // other attributes and settings as needed
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit;
 }
 
 
-// Establish a new connection to the database
 $mysqli = new mysqli($host, $username, $password, $dbname);
 
-// Check connection
 if ($mysqli->connect_error) {
-    // Consider logging this error and then displaying a generic error message to the user
     error_log("Connection failed: " . $mysqli->connect_error);
-    // Display a user-friendly message
     echo "Sorry, we're experiencing some technical difficulties. Please try again later.";
-    exit; // Stop script execution if connection fails
+    exit; 
+    
 }
