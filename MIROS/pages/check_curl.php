c4914ad6eea0b5397837d913dd2bbabae6855d39
Gceorge <george.colley@student.shu.ac.uk>
2024-04-13 11:20:48 +0100

    ignore the translate stuff for now its just a backup

---
+++ MIROS/pages/check_curl.php
@@ -0,0 +1,6 @@
+<?php
+if (function_exists('curl_init')) {
+    echo "cURL is enabled!";
+} else {
+    echo "cURL is not enabled!";
+}
---
+++ MIROS/pages/set_language.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (isset($_POST['lang'])) {
+    $_SESSION['language'] = $_POST['lang']; // Store the chosen language in the session
+    echo 'Language updated';
+} else {
+    echo 'No language provided';
+}
+?>
