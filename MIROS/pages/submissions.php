2b8bee3d1bc7aaca426d95cd7de7d48a1e56819a
LASawyer <161612784+LASawyer@users.noreply.github.com>
2024-04-19 13:03:56 +0100

    submissions.php

--- MIROS/pages/submissions.php
+++ MIROS/pages/submissions.php
@@ -96,8 +96,8 @@ function getSub(){
             </tbody>
         </table>
     </div>
-    <?php require("Script.php")?>
     <?php require_once __DIR__ . '/../includes/footer.php'; ?>
     </section>
 </body>
+<?php require("Script.php")?>
 </html>
