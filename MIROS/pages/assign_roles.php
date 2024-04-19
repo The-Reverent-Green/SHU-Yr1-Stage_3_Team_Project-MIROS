ddc0558c0d1fb0dbad9eceb9ff3f157580afa763
LASawyer <161612784+LASawyer@users.noreply.github.com>
2024-04-19 10:23:10 +0100

    assign_roles.php

--- MIROS/pages/assign_roles.php
+++ MIROS/pages/assign_roles.php
@@ -82,7 +82,7 @@ if(isset($mysqli) && $mysqli instanceof mysqli) {
             </div>
         </div>
     </section>
-    <?php require("Script.php")?>
 </body>
+<?php require("Script.php")?>
 <?php require_once __DIR__ . '/../includes/footer.php'; ?>
 </html>
